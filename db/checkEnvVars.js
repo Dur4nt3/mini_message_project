933e786895153baaccde572eaa8436e23036a212
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-12-24 16:51:24 +0200

    Update connection logic
    Improved the utility of "checkEnvVars.js".
    Additionally, changed the logic of "connectionURL.js".
    It will now use different connection URLs based on the environment.

--- db/checkEnvVars.js
+++ db/checkEnvVars.js
@@ -1,19 +1,30 @@
+const devEnvVars = [
+    'ROLE_NAME',
+    'DATABASE_PASSWORD',
+    'DATABASE',
+    'HOST',
+    'DATABASE_PORT',
+];
+
 export default function checkEnvVars() {
-    if (
-        (process.env.ROLE_NAME === undefined ||
-            process.env.DATABASE_PASSWORD === undefined ||
-            process.env.DATABASE === undefined ||
-            process.env.HOST === undefined ||
-            process.env.DATABASE_PORT === undefined) &&
-        process.env.DATABASE_URL === undefined
-    ) {
-        console.log('Role:', process.env.ROLE_NAME);
-        console.log('Database Password:', process.env.DATABASE_PASSWORD);
-        console.log('Database:', process.env.DATABASE);
-        console.log('Database Host:', process.env.HOST);
-        console.log('Database Port:', process.env.DATABASE_PORT);
+    if (process.env.DATABASE_URL) {
+        console.log('Railway database connection variable set, exiting.');
+        return;
+    }
+
+    const unsetVars = devEnvVars.filter((key) => !process.env[key]);
+
+    let additionalErrors;
+
+    if (unsetVars.length === devEnvVars.length) {
+        additionalErrors =
+            'All variables missing, meaning this is potentially a production environment without the railway variable';
+    }
+
+    if (unsetVars.length > 0) {
         throw new Error(
-            'App will not be able to connect to database, please check the above output!',
+            `Missing required environment variables: ${missing.join(', ')}`,
+            additionalErrors,
         );
     }
 }
--- db/connectionURL.js
+++ db/connectionURL.js
@@ -1,13 +1,16 @@
-import checkEnvVars from "./checkEnvVars.js";
+import checkEnvVars from './checkEnvVars.js';
 
 // The "DATABASE_URL environment variable is provided by Railway"
 // The other URL is used when in development
 
 const connectionURL =
-    process.env.DATABASE_URL ||
-    `postgresql://${process.env.ROLE_NAME}:${process.env.DATABASE_PASSWORD}@${
-        process.env.HOST
-    }:${Number(process.env.DATABASE_PORT)}/${process.env.DATABASE}`;
+    process.env.NODE_ENV === 'production'
+        ? process.env.DATABASE_URL
+        : `postgresql://${process.env.ROLE_NAME}:${
+              process.env.DATABASE_PASSWORD
+          }@${process.env.HOST}:${Number(process.env.DATABASE_PORT)}/${
+              process.env.DATABASE
+          }`;
 
 checkEnvVars();
 
